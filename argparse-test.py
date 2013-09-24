7aba72b5a8f85407afb0d4782234fdb5569afaa3
healthy kang <healthly@gmail.com>
2013-09-24 17:23:50 +0800

    modify

--- argparse-test.py
+++ argparse-test.py
@@ -10,7 +10,9 @@ from pymongo.errors import ConnectionFailure
 import argparse
 parser = argparse.ArgumentParser(description='analy nginx log from mongodb')
 
-parser.add_argument('-H', dest="dbhost" , help="mongodb's ip or hostname")
+parser.add_argument('-H', dest="dbhost", default='localhost', help="mongodb's ip or hostname")
+parser.add_argument('-N', dest="dbname", default='nginx111', help="mongodb's dbname")
+parser.add_argument('--ipurlnums', dest="ipurlnums", action='store_true', help="output ip and urls")
 #parser.add_argument('-N', dest="dbname", type=int)
 
-args = parser.parseargs()
+args = parser.parse_args()
