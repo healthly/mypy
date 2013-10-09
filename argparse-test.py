df1eb3f5bb5a1e1044203009567ea8ddbdec9976
healthy kang <healthly@gmail.com>
2013-10-09 09:46:01 +0800

    f

--- argparse-test.py
+++ argparse-test.py
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser(description='analy nginx log from mongodb')
 parser.add_argument('-H', dest="dbhost", default='localhost', help="mongodb's ip or hostname")
 parser.add_argument('-N', dest="dbname", default='nginx111', help="mongodb's dbname")
 parser.add_argument('--ipurlnums', dest="ipurlnums", action='store_true', help="output ip and urls")
+parser.add_argument('--int', dest='types', action='append_const', const=int)
 #parser.add_argument('-N', dest="dbname", type=int)
 
 args = parser.parse_args()
